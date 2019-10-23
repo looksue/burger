904d2ed38f2e82ecd51f383c6deff311dbfcd5c4
Sue Look <suelook1@gmail.com>
2019-10-23 08:14:08 -0400

    connection to mysql added

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+});
+
+// Make the connection to MySQL
+connection.connect(function(err) {
+    if (err) {
+      console.error('ERROR: MySQL connection error -- ' + err.stack + '\n\n');
+      return;
+    }
+    console.log('Connected to MySQL database as id ' + connection.threadId + '\n\n');
+  });
+  
+  // Export connection for ORM use
+  module.exports = connection;
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 var express = require ("express");
-var mysql = require("mysql");
 
 var app = express();
 var PORT = process.env.PORT || 8080;
@@ -10,10 +9,3 @@ app.use(express.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "burger_db"
-});
