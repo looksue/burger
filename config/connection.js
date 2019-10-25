c4adbda146ae251255dd716cce2e88d8b0afde77
Sue Look <suelook1@gmail.com>
2019-10-25 15:53:10 -0400

    Added JawsDB for running on Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,27 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+// setup database connection properties
+if (process.env.JAWSDB_URL) {
+  // Heroku uses JawsDB
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else { // otherwise use my PC
+  var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "root",
     database: "burgers_db"
-});
+  });
+};
 
-// Make the connection to MySQL
-connection.connect(function(err) {
+  // Make the connection to MySQL
+  connection.connect(function (err) {
     if (err) {
       console.error("ERROR: MySQL connection error -- " + err.stack + "\n\n");
       return;
     }
     console.log("Burger Bonanza is running" + "\n\n");
   });
-  
+
   // Export connection for ORM use
   module.exports = connection;
